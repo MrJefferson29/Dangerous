60eec3ac03b3309f1c303d7b4f282ef81bdda54d
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2023-12-14 01:37:39 +0100

    Update imageUpload.js

--- backend/Helpers/Libraries/imageUpload.js
+++ backend/Helpers/Libraries/imageUpload.js
@@ -1,45 +1,57 @@
-const CustomError = require("../error/CustomError");
-const multer = require("multer");
-const path = require("path");
+const CustomError = require("../error/CustomError")
+
+const multer = require("multer")
+const path = require("path")
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        // const rootDir = path.dirname(require.main.filename);
 
-        if (file.fieldname === "photo") {
-            cb(null, "./../../public/userPhotos");
-        } else {
-            cb(null, "./../../public/storyImages");
+    destination : function(req,file,cb ){
+        const rootDir = path.dirname(require.main.filename) 
+        
+        if(file.fieldname === "photo"){
+            cb(null , path.join( rootDir,"/public/userPhotos"))
         }
-    },
-
-    filename: function (req, file, cb) {
-        if (file.fieldname === "photo") {
-            const extensions = file.mimetype.split("/")[1];
-            req.savedUserPhoto = "photo_user_" + req.user.id + "." + extensions;
-            cb(null, req.savedUserPhoto);
-        } else {
-            req.savedStoryImage =
-                "image_" +
-                new Date().toISOString().replace(/:/g, "-") +
-                file.originalname;
-            cb(null, req.savedStoryImage);
+        else {
+            cb(null , path.join( rootDir,"/public/storyImages"))
         }
-    },
-});
 
-const fileFilter = (req, file, cb) => {
-    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif", "image/heif", "image/heic"];
+    } ,
 
-    if (!allowedMimeTypes.includes(file.mimetype)) {
-        // Pass the error as the first argument to the callback
-        return cb(new CustomError("Please provide a valid image file", 400), false);
+    filename :function(req,file,cb ) {
+
+       
+        if(file.fieldname ==="photo"){
+            const extentions =file.mimetype.split("/")[1]
+           
+            req.savedUserPhoto ="photo_user_" +req.user.id + "."+ extentions
+
+            cb(null ,req.savedUserPhoto)
+        }
+
+        else {
+            req.savedStoryImage ="image_" +new Date().toISOString().replace(/:/g, '-') + file.originalname 
+
+            cb(null ,req.savedStoryImage)
+        }
+
     }
 
-    // If the MIME type is allowed, pass null as the first argument
-    cb(null, true);
-};
+})
 
-const imageUpload = multer({ storage, fileFilter });
 
-module.exports = imageUpload;
+const fileFilter =(req,file,cb ) => {
+
+    allowedMimeTypes = ["image/jpeg","image/jpg","image/png","image/gif"]
+
+    if (!allowedMimeTypes.includes(file.mimetype)) {
+        return (new CustomError("Please provide a valid image file ",400),null )
+    }
+    
+    cb(null , true ) ;
+    
+}
+
+const imageUpload = multer({storage ,fileFilter  })
+
+
+module.exports = imageUpload ; 
