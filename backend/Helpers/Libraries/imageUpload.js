51afb9b08a3ca3a24583455b2168f892c22841ea
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2023-12-14 01:52:09 +0100

    Update imageUpload.js

--- backend/Helpers/Libraries/imageUpload.js
+++ backend/Helpers/Libraries/imageUpload.js
@@ -1,57 +1,36 @@
-const CustomError = require("../error/CustomError")
-
-const multer = require("multer")
-const path = require("path")
+const CustomError = require("../error/CustomError");
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const rootDir = path.dirname(require.main.filename);
+        const uploadDestination = file.fieldname === "photo" ? "/public/userPhotos" : "/public/storyImages";
+        cb(null, path.join(rootDir, uploadDestination));
+    },
 
-    destination : function(req,file,cb ){
-        const rootDir = path.dirname(require.main.filename) 
-        
-        if(file.fieldname === "photo"){
-            cb(null , path.join( rootDir,"/public/userPhotos"))
+    filename: function (req, file, cb) {
+        if (file.fieldname === "photo") {
+            const extension = file.mimetype.split("/")[1];
+            req.savedUserPhoto = `photo_user_${req.user.id}.${extension}`;
+            cb(null, req.savedUserPhoto);
+        } else {
+            req.savedStoryImage = `image_${new Date().toISOString().replace(/:/g, '-')}${file.originalname}`;
+            cb(null, req.savedStoryImage);
         }
-        else {
-            cb(null , path.join( rootDir,"/public/storyImages"))
-        }
-
-    } ,
-
-    filename :function(req,file,cb ) {
-
-       
-        if(file.fieldname ==="photo"){
-            const extentions =file.mimetype.split("/")[1]
-           
-            req.savedUserPhoto ="photo_user_" +req.user.id + "."+ extentions
-
-            cb(null ,req.savedUserPhoto)
-        }
-
-        else {
-            req.savedStoryImage ="image_" +new Date().toISOString().replace(/:/g, '-') + file.originalname 
-
-            cb(null ,req.savedStoryImage)
-        }
-
     }
+});
 
-})
-
-
-const fileFilter =(req,file,cb ) => {
-
-    allowedMimeTypes = ["image/jpeg","image/jpg","image/png","image/gif"]
+const fileFilter = (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
 
     if (!allowedMimeTypes.includes(file.mimetype)) {
-        return (new CustomError("Please provide a valid image file ",400),null )
+        cb(new CustomError("Please provide a valid image file", 400), false);
+    } else {
+        cb(null, true);
     }
-    
-    cb(null , true ) ;
-    
-}
+};
 
-const imageUpload = multer({storage ,fileFilter  })
+const imageUpload = multer({ storage, fileFilter });
 
-
-module.exports = imageUpload ; 
+module.exports = imageUpload;
